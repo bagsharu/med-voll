e19fdf82e811fc2b7408e772f960e81d532ed369
bagsharu <gabs.haru@gmail.com>
2024-12-23 16:33:58 -0300

    Corpo do método

--- api/src/main/java/bagsharu/voll/med/api/controller/MedicoController.java
+++ api/src/main/java/bagsharu/voll/med/api/controller/MedicoController.java
@@ -2,6 +2,7 @@ package bagsharu.voll.med.api.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @PostMapping
-    public void cadastrar() {
-
+    public void cadastrar(@RequestBody String json) {
+        System.out.println(json);
     }
 }
