fe491a22e4f40ab2f18da55d62ebf01c531fe03a
bagsharu <gabs.haru@gmail.com>
2025-01-02 15:02:07 -0300

    Informações para o Data Transfer Object de Cadastrar

--- api/src/main/java/bagsharu/voll/med/api/model/DadosCadastroMedico.java
+++ api/src/main/java/bagsharu/voll/med/api/model/DadosCadastroMedico.java
@@ -1,4 +1,4 @@
 package bagsharu.voll.med.api.model;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade) {
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
 }
---
+++ api/src/main/java/bagsharu/voll/med/api/model/DadosEndereco.java
@@ -0,0 +1,4 @@
+package bagsharu.voll.med.api.model;
+
+public record DadosEndereco(String logradouro, String bairro, String cep, String cidade, String uf, String complemento, String numero) {
+}
