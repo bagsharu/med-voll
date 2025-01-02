8c5f072ee2b904e4a6f7b5d1ec60c6cad23c667a
bagsharu <gabs.haru@gmail.com>
2025-01-02 16:23:40 -0300

    Repositório JPA para Medico

---
+++ api/src/main/java/bagsharu/voll/med/api/model/medico/MedicoRepository.java
@@ -0,0 +1,6 @@
+package bagsharu.voll.med.api.model.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico,Long> {
+}
