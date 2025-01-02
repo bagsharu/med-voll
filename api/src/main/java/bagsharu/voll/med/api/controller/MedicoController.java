f39e34c94908722c7d71e7a02f95a958f850528c
bagsharu <gabs.haru@gmail.com>
2025-01-02 16:24:19 -0300

    Injeção de dependências do repository

--- api/src/main/java/bagsharu/voll/med/api/controller/MedicoController.java
+++ api/src/main/java/bagsharu/voll/med/api/controller/MedicoController.java
@@ -1,6 +1,8 @@
 package bagsharu.voll.med.api.controller;
 
 import bagsharu.voll.med.api.model.medico.DadosCadastroMedico;
+import bagsharu.voll.med.api.model.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
 
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
