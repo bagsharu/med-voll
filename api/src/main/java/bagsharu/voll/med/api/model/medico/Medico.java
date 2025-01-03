27d16311317d7b9e7f6773c6f585318187e97967
bagsharu <gabs.haru@gmail.com>
2025-01-03 15:33:39 -0300

    Mapeando a especialidade

--- api/src/main/java/bagsharu/voll/med/api/model/medico/Medico.java
+++ api/src/main/java/bagsharu/voll/med/api/model/medico/Medico.java
@@ -33,6 +33,7 @@ public class Medico {
         this.nome = dados.nome();
         this.crm = dados.crm();
         this.email = dados.email();
+        this.especialidade = dados.especialidade();
 
         this.endereco = new Endereco(dados.endereco());
     }
