184a8367aca3e0101e207c2dfda1327b1505ba4d
bagsharu <gabs.haru@gmail.com>
2025-01-06 16:11:56 -0300

    Anotação para o campo Id ser obrigatório

--- api/src/main/java/bagsharu/voll/med/api/model/medico/DadosAtualizadosMedico.java
+++ api/src/main/java/bagsharu/voll/med/api/model/medico/DadosAtualizadosMedico.java
@@ -1,6 +1,7 @@
 package bagsharu.voll.med.api.model.medico;
 
 import bagsharu.voll.med.api.model.endereco.DadosEndereco;
+import jakarta.validation.constraints.NotNull;
 
-public record DadosAtualizadosMedico(Long id, String nome, String telefone, DadosEndereco endereco) {
+public record DadosAtualizadosMedico(@NotNull Long id, String nome, String telefone, DadosEndereco endereco) {
 }
