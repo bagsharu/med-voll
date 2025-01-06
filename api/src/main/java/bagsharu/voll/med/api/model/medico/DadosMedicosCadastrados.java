9a2711978d5cf2aa1fb065469b325efbf44d1581
bagsharu <gabs.haru@gmail.com>
2025-01-06 16:01:56 -0300

    O Data Transfer Object agora recebe o Id

--- api/src/main/java/bagsharu/voll/med/api/model/medico/DadosMedicosCadastrados.java
+++ api/src/main/java/bagsharu/voll/med/api/model/medico/DadosMedicosCadastrados.java
@@ -1,8 +1,8 @@
 package bagsharu.voll.med.api.model.medico;
 
-public record DadosMedicosCadastrados(String nome, String email, String crm, Especialidade especialidade) {
+public record DadosMedicosCadastrados(Long id, String nome, String email, String crm, Especialidade especialidade) {
 
     public DadosMedicosCadastrados(Medico medico) {
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
 }
