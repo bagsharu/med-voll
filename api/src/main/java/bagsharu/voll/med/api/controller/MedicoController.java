b40cb816f4ee8a40d25c6f41bd245018be24176d
bagsharu <gabs.haru@gmail.com>
2025-01-06 16:29:05 -0300

    Método DELETE, com variável de parâmetro

--- api/src/main/java/bagsharu/voll/med/api/controller/MedicoController.java
+++ api/src/main/java/bagsharu/voll/med/api/controller/MedicoController.java
@@ -42,4 +42,10 @@ public class MedicoController {
         medico.atualizarInformacoes(dados);
 
     }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        repository.deleteById(id);
+    }
 }
