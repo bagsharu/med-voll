78da142c7d2beac67ca0eb0072159f1e90a79901
bagsharu <gabs.haru@gmail.com>
2025-01-06 16:44:20 -0300

    Método Query para listar somente médicos ativos

--- api/src/main/java/bagsharu/voll/med/api/model/medico/MedicoRepository.java
+++ api/src/main/java/bagsharu/voll/med/api/model/medico/MedicoRepository.java
@@ -1,6 +1,9 @@
 package bagsharu.voll.med.api.model.medico;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicoRepository extends JpaRepository<Medico,Long> {
+    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 }
