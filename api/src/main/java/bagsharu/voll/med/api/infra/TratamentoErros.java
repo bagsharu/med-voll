b86feff32585a72afad5c6b341d859dad741750c
bagsharu <gabs.haru@gmail.com>
2025-01-07 16:52:37 -0300

    Corpo do método para tratar erros de validação, com um retorno mais legível. DTO (Record) para armazenar as informações da exceção.

--- api/src/main/java/bagsharu/voll/med/api/infra/TratamentoErros.java
+++ api/src/main/java/bagsharu/voll/med/api/infra/TratamentoErros.java
@@ -2,6 +2,7 @@ package bagsharu.voll.med.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,7 +18,21 @@ public class TratamentoErros {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity tratarErro400 () {
+    public ResponseEntity tratarErro400 (MethodArgumentNotValidException exception) {
 
+        // Armazena os erros em uma variável
+        var erros = exception.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+
+    }
+
+    // DTO contendo informações relevantes sobre o erro de validação
+    private record DadosErroValidacao(String campo, String mensagem) {
+
+        // Construtor recebe as informações do erro de validação, com o campo e a mensagem referente
+        public DadosErroValidacao(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
