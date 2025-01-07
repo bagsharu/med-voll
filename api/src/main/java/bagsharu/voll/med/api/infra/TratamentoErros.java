8643d47a391bc4b780403330a426ae322b34578c
bagsharu <gabs.haru@gmail.com>
2025-01-07 16:35:00 -0300

    Método para tratar erros 404

--- api/src/main/java/bagsharu/voll/med/api/infra/TratamentoErros.java
+++ api/src/main/java/bagsharu/voll/med/api/infra/TratamentoErros.java
@@ -1,7 +1,17 @@
 package bagsharu.voll.med.api.infra;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class TratamentoErros {
+
+    // Anotação Spring para sinalizar que este metodo trata erros "Not Found"
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro4040 (){
+
+        return ResponseEntity.notFound().build();
+    }
 }
