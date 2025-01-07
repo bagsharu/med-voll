3f29e7d9e7073096a09a6dfad534fb5f7d616c45
bagsharu <gabs.haru@gmail.com>
2025-01-07 16:38:18 -0300

    Assinatura do metodo para tratar o erro400

--- api/src/main/java/bagsharu/voll/med/api/infra/TratamentoErros.java
+++ api/src/main/java/bagsharu/voll/med/api/infra/TratamentoErros.java
@@ -2,6 +2,7 @@ package bagsharu.voll.med.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,8 +11,13 @@ public class TratamentoErros {
 
     // Anotação Spring para sinalizar que este metodo trata erros "Not Found"
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity tratarErro4040 (){
+    public ResponseEntity tratarErro404 (){
 
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400 () {
+
     }
 }
