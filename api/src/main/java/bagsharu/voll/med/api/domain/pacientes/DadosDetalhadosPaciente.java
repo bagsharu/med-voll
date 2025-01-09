1099c5c8d218191ae26d3336b874b7c64bdfa314
bagsharu <gabs.haru@gmail.com>
2025-01-09 10:24:43 -0300

    Corrigir nome

--- api/src/main/java/bagsharu/voll/med/api/domain/pacientes/DadosDetalhadosPaciente.java
+++ api/src/main/java/bagsharu/voll/med/api/domain/pacientes/DadosDetalhadosPaciente.java
@@ -2,8 +2,8 @@ package bagsharu.voll.med.api.domain.pacientes;
 
 import bagsharu.voll.med.api.domain.endereco.Endereco;
 
-public record DadosDetaljadosPaciente(Long id, String nome, String email, String cpf, String telefone, Endereco endereco) {
-    public DadosDetaljadosPaciente(Paciente paciente) {
+public record DadosDetalhadosPaciente(Long id, String nome, String email, String cpf, String telefone, Endereco endereco) {
+    public DadosDetalhadosPaciente(Paciente paciente) {
         this(paciente.getId(),paciente.getNome(),paciente.getEmail(),paciente.getCpf(),paciente.getTelefone(),paciente.getEndereco());
     }
 }
