f33f6f8f67ae1b8d99e4611b875410e692e315d6
bagsharu <gabs.haru@gmail.com>
2025-01-09 10:52:14 -0300

    Método GET para listar pacientes

--- api/src/main/java/bagsharu/voll/med/api/controller/PacienteController.java
+++ api/src/main/java/bagsharu/voll/med/api/controller/PacienteController.java
@@ -1,5 +1,6 @@
 package bagsharu.voll.med.api.controller;
 
+import bagsharu.voll.med.api.domain.medico.DadosMedicosCadastrados;
 import bagsharu.voll.med.api.domain.pacientes.DadosCadastroPaciente;
 import bagsharu.voll.med.api.domain.pacientes.DadosDetalhadosPaciente;
 import bagsharu.voll.med.api.domain.pacientes.Paciente;
@@ -7,11 +8,11 @@ import bagsharu.voll.med.api.domain.pacientes.PacienteRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -36,5 +37,15 @@ public class PacienteController {
 
         // Retorna o código 201, a uri e os dados enviados e cadastrados no banco de dados
         return ResponseEntity.created(uri).body(new DadosDetalhadosPaciente(paciente));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosDetalhadosPaciente>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+
+        // Retorna uma lista de DadosCadastroPaciente com base no banco de dados, o map é para
+        // converter de Medico para o DTO.
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosDetalhadosPaciente::new);
+        
+        return ResponseEntity.ok(page);
     }
 }
