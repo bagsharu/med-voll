fdefd8661475ce94f761eb0878703b5684692945
bagsharu <gabs.haru@gmail.com>
2025-01-09 10:06:07 -0300

    Anotações de mapeamento de Tabela e Entidade, anotações Lombok para Getters e construtores

--- api/src/main/java/bagsharu/voll/med/api/domain/pacientes/Paciente.java
+++ api/src/main/java/bagsharu/voll/med/api/domain/pacientes/Paciente.java
@@ -1,5 +1,17 @@
 package bagsharu.voll.med.api.domain.pacientes;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Table(name="pacientes")
+@Entity(name="Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Paciente {
 }
