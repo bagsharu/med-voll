7227292f935a94764581613926e9bf75ed0d942e
bagsharu <gabs.haru@gmail.com>
2025-01-09 10:12:48 -0300

    Atributos da classe/entidade paciente

--- api/src/main/java/bagsharu/voll/med/api/domain/pacientes/Paciente.java
+++ api/src/main/java/bagsharu/voll/med/api/domain/pacientes/Paciente.java
@@ -1,11 +1,13 @@
 package bagsharu.voll.med.api.domain.pacientes;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+
+import bagsharu.voll.med.api.domain.endereco.Endereco;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Table(name="pacientes")
 @Entity(name="Paciente")
@@ -14,4 +16,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String cpf;
+
+    @Embedded
+    private Endereco emdereco;
 }
