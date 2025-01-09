a8ff680bac65449f81dedb5d860e2bcc1629ab01
bagsharu <gabs.haru@gmail.com>
2025-01-09 09:58:18 -0300

    Primeiro DTO para pacientes

---
+++ api/src/main/java/bagsharu/voll/med/api/domain/pacientes/DadosCadastroPaciente.java
@@ -0,0 +1,14 @@
+package bagsharu.voll.med.api.domain.pacientes;
+
+import bagsharu.voll.med.api.domain.endereco.Endereco;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosCadastroPaciente(@NotBlank String nome,
+                                    @NotBlank @Email String email,
+                                    @NotBlank String telefone,
+                                    @NotBlank String cpf,
+                                    @NotNull @Valid Endereco endereco) {
+}
