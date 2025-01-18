15dd8df46bba4faacbc675c733fbf44474be324c
bagsharu <gabs.haru@gmail.com>
2025-01-18 18:32:34 -0300

    Configuração para receber senhas criptografadas em BCrypt

--- api/src/main/java/bagsharu/voll/med/api/controller/AutenticacaoController.java
+++ api/src/main/java/bagsharu/voll/med/api/controller/AutenticacaoController.java
@@ -1,9 +1,26 @@
 package bagsharu.voll.med.api.controller;
 
+import bagsharu.voll.med.api.domain.usuario.DadosAutenticacao;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login")
 public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+
+        // Converte informações do DTO login/senha para os dados recebidos pelo manager
+        var token = new UsernamePasswordAuthenticationToken(dados.login(),dados.senha());
+        var auth = manager.authenticate(token);
+    }
 }
