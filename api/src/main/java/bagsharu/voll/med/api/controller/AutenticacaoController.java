fa4552dd8104f79bc506a05a0f977acecd0cb5f7
bagsharu <gabs.haru@gmail.com>
2025-01-19 13:19:20 -0300

    Anotação do método do controller

--- api/src/main/java/bagsharu/voll/med/api/controller/AutenticacaoController.java
+++ api/src/main/java/bagsharu/voll/med/api/controller/AutenticacaoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
 
         // Converte informações do DTO login/senha para os dados recebidos pelo manager
