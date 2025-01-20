2029822b3a36284426eea2ef3d04e16da9a0f3d1
bagsharu <gabs.haru@gmail.com>
2025-01-20 15:35:50 -0300

    Anotação para aceitar a variável de ambiente

--- api/src/main/java/bagsharu/voll/med/api/infra/security/TokenService.java
+++ api/src/main/java/bagsharu/voll/med/api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import bagsharu.voll.med.api.domain.usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -13,6 +14,7 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
+    @Value("api.security.token.secret")
     private String secret;
 
     public String gerarToken(Usuario usuario) {
