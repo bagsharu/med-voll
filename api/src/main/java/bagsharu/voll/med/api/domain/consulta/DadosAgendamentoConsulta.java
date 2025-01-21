43284047eecc57daa6b44f77822d1e2fe59f5e31
bagsharu <gabs.haru@gmail.com>
2025-01-21 12:11:42 -0300

    Campo Especialidade

--- api/src/main/java/bagsharu/voll/med/api/domain/consulta/DadosAgendamentoConsulta.java
+++ api/src/main/java/bagsharu/voll/med/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -1,9 +1,10 @@
 package bagsharu.voll.med.api.domain.consulta;
 
+import bagsharu.voll.med.api.domain.medico.Especialidade;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
-public record DadosAgendamentoConsulta(Long idMedico, @NotNull Long idPaciente, @NotNull @Future LocalDateTime data) {
+public record DadosAgendamentoConsulta(Long idMedico, @NotNull Long idPaciente, @NotNull @Future LocalDateTime data, Especialidade especialidade) {
 }
