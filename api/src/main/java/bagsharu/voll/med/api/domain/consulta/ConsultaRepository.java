03b922e11a6a8b5953235289e6d4018d1df0949f
bagsharu <gabs.haru@gmail.com>
2025-01-21 12:41:27 -0300

    Métodos Query para validações

--- api/src/main/java/bagsharu/voll/med/api/domain/consulta/ConsultaRepository.java
+++ api/src/main/java/bagsharu/voll/med/api/domain/consulta/ConsultaRepository.java
@@ -1,6 +1,13 @@
 package bagsharu.voll.med.api.domain.consulta;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    Boolean existByMedicoIdAndData(Long aLong, @NotNull @Future LocalDateTime data);
+
+    Boolean existsByPacienteIdAndDataBetween(@NotNull Long aLong, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
 }
--- api/src/main/java/bagsharu/voll/med/api/domain/medico/MedicoRepository.java
+++ api/src/main/java/bagsharu/voll/med/api/domain/medico/MedicoRepository.java
@@ -24,4 +24,12 @@ public interface MedicoRepository extends JpaRepository<Medico,Long> {
                        limit 1
            """)
     Medico escolherMedicoAleatorio(Especialidade especialidade);
+
+    @Query("""
+            select m.ativo
+            from Medico e
+            where
+            m.id = :id
+            """)
+    Boolean findAtivoById(Long aLong);
 }
