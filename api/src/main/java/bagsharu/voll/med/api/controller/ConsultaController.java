b8cd2088f668f3ed4d70a7287bc7b7786b21c5b1
bagsharu <gabs.haru@gmail.com>
2025-01-21 11:37:14 -0300

    Método POST para agendar consultas

--- api/src/main/java/bagsharu/voll/med/api/controller/ConsultaController.java
+++ api/src/main/java/bagsharu/voll/med/api/controller/ConsultaController.java
@@ -1,5 +1,10 @@
 package bagsharu.voll.med.api.controller;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("consultas")
 public class ConsultaController {
 
-
+    @PostMapping
+    @Transactional
+    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+        System.out.println(dados);
+        return ResponseEntity.ok(new DadosDetalhadosConsulta());
+    }
 }
