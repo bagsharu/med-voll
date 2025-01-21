4dd493e06cd988a96e3e4306e5e565a0383de077
bagsharu <gabs.haru@gmail.com>
2025-01-21 12:02:29 -0300

    Construtor

--- api/src/main/java/bagsharu/voll/med/api/domain/consulta/Consulta.java
+++ api/src/main/java/bagsharu/voll/med/api/domain/consulta/Consulta.java
@@ -3,9 +3,12 @@ package bagsharu.voll.med.api.domain.consulta;
 import bagsharu.voll.med.api.domain.medico.Medico;
 import bagsharu.voll.med.api.domain.pacientes.Paciente;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Table(name = "consultas")
 @Entity(name = "Consulta")
@@ -25,6 +28,13 @@ public class Consulta {
     private Paciente paciente;
 
     private LocalDateTime data;
+
+    public Consulta(Long id, Medico medico, Paciente paciente, LocalDateTime data) {
+        this.id = id;
+        this.medico = medico;
+        this.paciente = paciente;
+        this.data = data;
+    }
 
     public Long getId() {
         return id;
